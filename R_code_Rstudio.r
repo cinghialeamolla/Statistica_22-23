cd14b7f2948da2fe8d37e542d4dac7d50d149a88
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-10-11 12:50:02 +0200

    Update R_code_Rstudio.r

--- R_code_Rstudio.r
+++ R_code_Rstudio.r
@@ -1,8 +1,54 @@
 
 
-library(tidyverse) #per caricare i diversi pacchetti istallati
+#library(tidyverse) #per caricare i diversi pacchetti istallati
 
-setwd("C:/Runibo/") # Windows x caricare la directory
-getwd() #
+#setwd("C:/Runibo/") # Windows x caricare la directory
+#getwd() #
+
+#11/10/2022 
+#Spiegaz su Rstudio
+#programma: istruzioni da dare al computer, anche una singola linea di codice
+#uno script è un documento di testo che contiene istruzioni, si apre con il + su RStudio
+
+### PARTE 
+#scrivere "Hello Word"
+print("Hello World!")   #funzione che stampa su schermo l'argomento, che va tra parentesi. Equivale a un verbo.
+
+#per eseguire il programma si usa "run"
+#i tasti "ctr+ent" ripetono il comando sopra
+#[n]: indici
+# > "pront": significa che si è su una nuova riga di codice
+
+### PARTE 2
+#5^2 -> 5 elevato alla seconda, si può scrivere anche 5**2
+#9^(1/2)  -> radice quadrata di 9
+#27^(1/3) -> radice cubica di 27
+#5 %% 2  -> il resto nella divisione
+#5 %/% 2  ->
+
+#un operatore è un simbolo che agisce come una funzione in R
+#ci sono anche op. logici, relazionari, vari, assignement operator
+
+### PARTE 3
+#salvare uno script SU Rstudio#
+#selezionaare il testo da salvare poi "ctrl shift + c"
+
+# assegnazione variabili: creare una variabile e darle un valore
+variable <- "value"
+# nuova funzione
+my_name <- "Pamela"
+#
+v <- "october"
+v <- "november" #per aggiornare il contenuto di una variabule
+print(v) # trova info sulla funzione, in questo caso il valore
+
+great <_ "Hello World!"
+print(great)
+
+#c1 è un nomne, 1c no
+
+
+
+
 
 
