9051e1e65995822deebe51add04ddb9d6d165476
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-10-18 13:00:11 +0200

    Update R_code_Rstudio.r

--- R_code_Rstudio.r
+++ R_code_Rstudio.r
@@ -51,4 +51,75 @@ print(great)
 #altrimenti si può porre come domanda, es: "how to assign a variable inR"
 
 
+### TERZA LEZIONE (infarinata sulla sintassi)
+# come cambiare layout (disposizione pain e tab in RStudio [i 4 macropannelli]):
+#"workspace pins" -> "pane layout" 
+
+variable <- "some value"  #per assegnare una variabile
+variable <- "another value" #per cambiare assegnazione
+variable #scrive la variabile
+print(variable) #utile per chiarezza, per capire cosa fa quella linea di codice, si mette prima del codice: stampa il contenuto della variabile
+print("variable") #stampa il valore letterale, con le virgolette
+(my_logical <- TRUE) #tra parentesi si ottiene sia l'assegnazione che la stampa del contenuto
+
+#le variabili si possono usare nelle funzioni, o come valori
+my_num <- 3
+my_num *2
+my_multiplayer <- 2
+(my_num*my_multiplayer) #restituisce il valore tra la moltiplicazione tra le due variabili
+
+var_to_be_updated <- 72
+var_to_be_updated <- var_to_be_updated-2
+var_to_be_updated   #modificare e fare operazioni con le variabili
+
+#6 tipi di numeri
+#logical (tipo logico): TRUE-FALSE/T-F/O-1
+#sono keyword che si usano solo maiuscole, e non vanno assegnate a variabili. Non vengono racchiusi da virgolette. Sono sempre solo 2 valori
+
+#integer (numeri interi): 1L
+#si usa "L" dopo il numero per indicare cheè intero
+
+#numeric (float/double precision/numeri decimali): 1.0
+#numeri separati dal "." (perchè la "," serve a separare argomenti)
+
+#character   #contengono parole, ma non solo. Anche altri simboli, qualsiasi cosa purchè siano tra virgolette e verranno sempreusati come parole.
+
+#complex (numeri complessi)
+
+#raw (dati binari, di sistema)
+
+typeof(1L) #integer #dice i "tipi"
+typeof(1) #double
+class(1L) #integer  #funzione più approfondita dell'altra
+class (1) #numeric
+
+first_word <-  "hello"
+second_word <-  "word"
+first_word
+second_word
+paste(first_word, second_word)  #unico caracter dalle due variabili, il risultato dà solo una coppia di virgolette
+paste(first_word, second_word, sep = "_") #specificare cosa usare per separare i valori delle due variabili
+
+help("print") #vedere da quale pacchetto viene, apre una scheda sul tutorial, mostra anche come la funz viene usata
+?sum
+
+## pacchetti: alcuni solo per certi sistemi operativi. Biogna installarli
+#per installare un pacchetto sul CRAN usare la funzione sottostante 
+install.packages("vegan") #si fa solo una volta
+library(vegan) #per richiamare il pacchetto ad ogni nuova sessione, e accedere alle funzionalità
+
+#nome completo di una funzione
+#base::sum() <-  nome completo funzione,con prima il pacchetto. Serve per le funzioni che hanno stesso nome in due pacchetti diversi.
+#dplyr::select
+#raster::select
+
+
+
+
+
+
+
+
+
+
 
