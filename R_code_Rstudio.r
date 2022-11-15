c03f5f1e083d3f96b02d0079ce9fbff824e9065e
Pamela Sbardella <80597231+cinghialeamolla@users.noreply.github.com>
2022-11-15 12:58:40 +0100

    Update R_code_Rstudio.r

--- R_code_Rstudio.r
+++ R_code_Rstudio.r
@@ -114,6 +114,173 @@ library(vegan) #per richiamare il pacchetto ad ogni nuova sessione, e accedere a
 #raster::select
 
 
+### QUARTA LEZIONE
+
+
+
+
+### QUINTA LEZIONE
+
+
+
+
+### SESTA LEZIONE
+
+m <- matrix(1:16, nrow=4)  #crea una matrice di dati
+rownames(m) <- c("r1", "r2", "r3", "r4")  #nominare le righe delle matrici
+colnames(m) <- c("c1", "c2", "c3", "c4")  #nominare le colonne delle matrici
+m
+m+2
+m*2
+
+n <- matrix(8:15, nrow=4)  #crea una matrice di dati
+n
+m %*% n  #operazione tra matrici
+
+vec <- 1:9
+sum(vec)
+sum(m)  #somma i valori
+mean(m) #calcola la media
+median(m) #trova la mediana
+sd(m) #
+rowSums(m)    #per sommare i valori lungo le righe
+colSums(m)    #per sommare i valori lungo le colonne
+rowMeans(m)   #media dei valori delle singole righe
+colMeans(m)   #media dei valori delle singole colonne
+
+m[1, ]  #per estrarre la prima riga
+sum(m[1, ])  #somma la I riga
+
+#Fattori: sono tipi speciali di vettori, possono essere ORDINATI o no. Vettori "integer" 
+
+vec <- c("Tree", "Grass", "Forb", "Grass", "Tree")    #crea un vettore
+vec   #mostra gli elementi del vettore, tra virgolette
+fct <- factor(vec)    #crea un fattore 
+fct   #mostra i valori del vettore senza virgolette, e li "ordina" in "levels"
+#levels: modalità/valori che può assumere una variabile; nel nostro vettore "Tree, Grass e Forb"
+
+class(fct)  #fattore
+typeof(fct)   #integer
+as.integer(fct)   #assegna livelli ai valori, mostrando i label (etichette) ad essi associate.
+# l'ordine automatico è quello alfabetico, a meno che non ne assegnamo uno diverso noi
+
+lev <- c("Tree", "Grass", "Forb", "Liana")        #definisce le modalità
+fct <- factor(vec, levels = lev)       #assegnare le possibili modalità ai fattori
+fct
+
+vec1 <- c("Tree", "Grass", "Macrophyte")   #nuovo vettore
+fct1 <- factor(vec1, levels=lev)    #trasformazione in vettore con modalità assegnate
+fct1    # "Macrophyte" non è stato definito, quindi il fattore restituisce <NA>
+
+summary(fct)    #vede le frequenze delle modalità
+levels(fct)    #vede le possibili modalità
+
+##fattori ordinati:
+vec <- c("High Temp", "Low Temp", "High Temp","Low Temp", "Medium Temp")
+fct <- factor(vec, ordered = TRUE, levels = c("Low Temp", "Medium Temp", "High Temp")) #creare un vettore con l'ordine
+fct
+
+#LIST                                 #sono collezioni ordinate di valori
+#Vettori, Matrici, ecc    
+
+student <- "piero"      #crea una lista
+scores <- c(27, 29, 23, 30)   #vettore numerico in lista
+is_enrolled <- TRUE   #
+my_list <- list(student, scores, is_enrolled)  #crea la lista
+my_list
+
+# [[1]]           #indice tra doppia parentesi quadra, indica i vari vettori
+# [1] "piero"
+
+# [[2]]
+# [1] 27 29 23 30   #vettore
+
+# [[3]]
+# [1] TRUE
+
+lenghth(my_list)   #lunghezza lista (3 vettori)
+class(my_list)
+typeof(my_list)
+str(my_list)   #mostra gli elementi che compongono la lista
+
+list_names <- c("student", "scores", "is_enrolled")                  #dare un nome alle liste
+names(my_list) <- list_names                                         #assegna il vettore list_names alla lista
+
+c(a=1, b=2)     #crea vettore con nomi assegnati
+
+#si possono definire i nomi associati anche manualmente
+list(people=student, grades=scores, is_enrolled=is_enrolled)
+
+my_list[1]   
+class(my_list[1])  #estraggo una o più lista che contiene le componenti selezionate (non estrae una componente)
+my_list[[1]]       #estrae una componente da una lista 
+class(my_list[[1]])  #
+
+my_list["student"]        #estrae 
+
+my_list$student #si riferisce a una singola componente da una lista
+class(my_list$student)
+my_list[c(1,3)]
+
+student <- "piero"
+scores <- c(t1=27, t2=29, t3=23)
+is_enrolled <- FALSE
+my_list <- list(student = student, scores = scores, is_enrolled = is_enrolled)
+my_list
+str(my_list)        #non so cosa fa
+
+my_list[[c(2,3)]] #estrae qualcosa
+my_list[[c("scores","t3")]] #estrae la stessa cosa
+my_list[[2]]   #
+
+#modificare le liste
+my_list[[2]][3] <- 24    #cambia il terzo valore della seconda lista
+my_list
+
+my_list[["course"]] <- "natural sciences"  #aggiunge una nuova componente dopo l'ultimo elemento
+my_list  
+my_list$internship <- FALSE
+my_list
+my_list[[6]] <- "B2"   #usando un numero da 1 a 5 andrebbe a sostituire una lista presente
+
+names(my_list)[6] <- "english certification"  #assegna un nome alla sesta lista
+
+
+#rimuovere gli errori (liste vuote)
+
+my_list <- my_list[-7]  #indice negativo sulla lista da eliminare
+my_list[[-7]] <- NULL   #usare null sulla lista da eliminare
+
+#concatenare le liste
+lst_1 <- list(a1 = 1:19, b1 = LETTERS[1:3])
+lst_2 <- list(a2 = c(2.2, 3.1), b2 = c(TRUE, FALSE))
+                                                      #####NON HO IDEA DI COSA SIA #######
+lst_3 <- c(lst_1, lst_2)
+str(lst_3)
+
+lst_4 <- list(c1 = lst_1, c2 = lst_2)
+str(lst_4)
+
+#DATAFRAME: struttura bidimensionale che può contenere qualsiasi vettore, con lo stesso tipo sulle colonne e tipi diversi sulle righe
+#un dataframe è una lista speciale (come una matrice è un tipo particolare di vettore)
+#dataframe:
+#1) ogni componente è un qualsiasi tipo di vettore (anche fattore) e corrisponde a una lista
+#2) le colonne/componenti DEVONO avere tutte nomi
+#3) le colonne devono avere tutte la stessa lunghezza
+
+#Creare un dataframe
+dat <- data.frame(a = 1:10, b = LETTERS [1:10], C = rep(C(TRUE, FALSE), 5))
+dat
+dim(dat)
+nrow(dat)          #righe
+ncol(dat)         #colonne
+class(dat)        #classe
+typeof(dat)        #tipo
+str(dat)          #dice osservazioni e variabili
+names(dat)         #dice i nomi delle colonne
+names(dat) <- c("col1", "col2", "col3")       #cambiare i nomi
+
+
 
 
 
